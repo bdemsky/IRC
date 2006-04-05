70b850cf551e8d8cb287a8b0e2d93c204f26b3e4
bdemsky <bdemsky>
2006-04-05 22:09:52 +0000

    Runtime code

---
+++ Robust/src/Runtime/runtime.c
@@ -0,0 +1,10 @@
+#include "runtime.h"
+#include "structdefs.h"
+
+int ___Object______hashcode____(struct ___Object___ * ___this___) {
+  return (int) ___this___;
+}
+
+void ___System______printInt____I(int x) {
+  printf("%d\n",x);
+}
