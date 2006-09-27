afa9e41f5bbc3abee3d020ddfa2922d1fec64a86
bdemsky <bdemsky>
2006-09-27 16:38:03 +0000

    command line test

---
+++ Robust/src/Tests/CommandLineTest.java
@@ -0,0 +1,8 @@
+class CommandLineTest {
+    public static void main(String str[]) {
+	for(int i=0;i<str.length;i++) {
+	    System.printString(str[i]);
+	    System.printString("\n");
+	}
+    }
+}
