e1e08ccf24c7666cfa341f2812416c5661840e92
bdemsky <bdemsky>
2006-10-31 00:40:07 +0000

    cover stuff with test cases

--- Robust/src/Tests/StringTest.java
+++ Robust/src/Tests/StringTest.java
@@ -2,5 +2,12 @@ class StringTest {
     public static void main(String str[]) {
 	String a="hello world\n";
 	System.printString(a);
+	System.printInt(a.indexOf('e'));
+	System.printString("\n");
+	System.printInt(a.indexOf("world"));
+	System.printString("\n");
+	System.printString(a.subString(3));
+	System.printString(a.subString(3,6));
+	System.printString("\n");
     }
 }
