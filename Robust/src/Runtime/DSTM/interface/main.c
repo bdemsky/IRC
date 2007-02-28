9cb94a37b8422c345e175fe03a39b99d825a9180
adash <adash>
2007-02-28 01:53:39 +0000

    test file for dstm functions

---
+++ Robust/src/Runtime/DSTM/interface/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "dstm.h"
+
+#define size 1000000
+	
+
+obj_addr_table_t mlut;
+int classsize[]={sizeof(int),sizeof(char),sizeof(short), sizeof(void *)};	
+
+int main() {	
+	int i;
+	
+	dstm_init();
+	create_objstr(size);
+	createHash(&mlut, HASH_SIZE, 0.75);	
+	
+	for(i=0; i< 4 ; i++) {
+		createObject(i);
+	}
+	
+	createObject(3);
+	return 0;
+}
