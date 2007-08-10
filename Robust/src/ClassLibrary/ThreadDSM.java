8d253621d181938092b8db0be0bd919c1b909470
bdemsky <bdemsky>
2007-08-10 08:03:28 +0000

    check in code

---
+++ Robust/src/ClassLibrary/ThreadDSM.java
@@ -0,0 +1,15 @@
+public class Thread {
+    public void start() {
+	nativeCreate();
+    }
+
+    private static void staticStart(Thread t) {
+	t.run();
+    }
+
+    public native static void sleep(long millis);
+    
+    public void run() {}
+
+    private native void nativeCreate();
+}
