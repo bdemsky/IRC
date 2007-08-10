91e668de647159ef8061095adc0546905af81ad7
bdemsky <bdemsky>
2007-08-10 07:59:50 +0000

    DSM code

---
+++ Robust/src/ClassLibrary/ObjectJavaDSM.java
@@ -0,0 +1,30 @@
+public class Object {
+    public int cachedCode; //first field has to be a primitive
+    public boolean cachedHash;
+    public Object nextobject; /* Oid */
+    public Object localcopy;
+
+    public native int nativehashCode();
+
+    public int hashCode() {
+	if (!cachedHash) {
+	    cachedCode=nativehashCode();
+	    cachedHash=true;
+	}
+	return cachedCode;
+    }
+
+    /* DON'T USE THIS METHOD UNLESS NECESSARY */
+    /* WE WILL DEPRECATE IT AS SOON AS INSTANCEOF WORKS */
+    public native int getType();
+
+    public String toString() {
+	return String.valueOf(this);
+    }
+
+    public boolean equals(Object o) {
+	if (o==this)
+	    return true;
+	return false;
+    }
+}
