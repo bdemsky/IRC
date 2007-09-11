d2a6b3d2559edfaf16768ac63dd6a27a00cea144
adash <adash>
2007-09-11 23:02:49 +0000

    Correct the file ..incorrect checkin the first time

--- Robust/src/ClassLibrary/ThreadDSM.java
+++ Robust/src/ClassLibrary/ThreadDSM.java
@@ -1,14 +1,14 @@
-public class ThreadDSM {
+public class Thread {
 
 	public void start(int mid) {
-		run(mid);
+		remotethreadstart(mid);
 	}
 
 	public native static void sleep(long millis);
 
-	public void run(int mid) {
+	public void run() {
 
 	}
 	
-	public int startRemoteThread(int mid);
+	public  native void remotethreadstart(int mid);
 }
