aba86d2ffefda0672651a7e13b8c5fd6c30a042f
bdemsky <bdemsky>
2007-09-13 09:21:16 +0000

    remove hashfunction/equals methods

--- Robust/src/Analysis/TaskStateAnalysis/EGEdge.java
+++ Robust/src/Analysis/TaskStateAnalysis/EGEdge.java
@@ -8,29 +8,11 @@ import Util.Edge;
 
 
 public class EGEdge extends Edge{
-    EGTaskNode target;
-
-    
     public EGEdge(EGTaskNode target){
 	super(target);
-	this.target = target;
     }
 
     public EGTaskNode getTarget(){
-	return target;
+	return (EGTaskNode) target;
     }
-		 
-    public int hashCode(){
-	return target.hashCode();
-    }
-    	
-    public boolean equals(Object o) {
-        if (o instanceof EGEdge) {
-            EGEdge e=(EGEdge)o;
-	    return e.target.equals(target);
-        }
-        return false;
-    }
-
-    
 }
