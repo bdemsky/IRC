73bbc7faefe79616919fa90400eb40aaabd1442f
bdemsky <bdemsky>
2007-09-14 09:38:26 +0000

    start real remote thread

--- Robust/src/Tests/remotethreadtest.java
+++ Robust/src/Tests/remotethreadtest.java
@@ -1,14 +1,22 @@
 public class RemoteThread extends Thread {
-	public RemoteThread() {
+    public RemoteThread() {
+    }
+    
+    public static void main(String[] st) {
+	int mid = (128<<24)|(200<<16)|(9<<8)|26;
+	RemoteThread t =null;
+	atomic {
+	    t= global new RemoteThread();
 	}
+	System.printString("Starting");
+	t.start(mid);
+	System.printString("Finished");
+	//this is ugly...
+	while(true) ;
+    }
 
-	public static void main(String[] st) {
-		int mid = 127;
-		RemoteThread t =null;
-		atomic {
-		    t= global new RemoteThread();
-		}
-		t.start(mid);
-	}
+    public int run() {
+	System.printString("Remote machine");
+    }
 }
 
