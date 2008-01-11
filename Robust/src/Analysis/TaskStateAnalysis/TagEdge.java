f3cf1d11e7f8622459bc99ab818311d10e66d49d
bdemsky <bdemsky>
2008-01-11 07:09:26 +0000

    add another class

---
+++ Robust/src/Analysis/TaskStateAnalysis/TagEdge.java
@@ -0,0 +1,39 @@
+package Analysis.TaskStateAnalysis;
+import IR.*;
+import Analysis.TaskStateAnalysis.*;
+import IR.Tree.*;
+import IR.Flat.*;
+import java.util.*;
+import Util.Edge;
+
+/* Edge *****************/
+
+public class TagEdge extends Edge {
+
+    private TaskDescriptor td;
+    /** Class Constructor
+     * 
+     */
+    public TagEdge(TagState target, TaskDescriptor td) {
+	super(target);
+	this.td=td;
+    }
+    
+    public int hashCode(){
+	return target.hashCode()^td.hashCode();
+    }
+
+    public TaskDescriptor getTask() {
+	return td;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof TagEdge) {
+            TagEdge e=(TagEdge)o;
+	    if (e.target.equals(target)&&
+		e.td==td)
+		return true;
+        }
+        return false;
+    }
+}
