aff730d651ce858899a138094f6b2cada38e6713
sivaji <sivaji>
2008-02-20 23:03:42 +0000

    *** empty log message ***

--- Robust/src/Benchmarks/Conglomerator/Tag/Stock.java
+++ Robust/src/Benchmarks/Conglomerator/Tag/Stock.java
@@ -1,8 +1,8 @@
 public class Stock extends Lookup {
 	public Stock() {
-		//url="q?s=%5EDJI";
-		url="";
-		hostname="www.yahoo.com";
+		url="q?s=%5EDJI";
+		//url="";
+		hostname="finance.yahoo.com";
 		start="</title>";
 		end="</html>";
 		exclusive=true;
