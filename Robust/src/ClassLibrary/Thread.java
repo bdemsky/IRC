8cb58d3f2a923715ae372b432063fa57a9322564
bdemsky <bdemsky>
2008-04-14 21:41:21 +0000

    try to add join support

--- Robust/src/ClassLibrary/Thread.java
+++ Robust/src/ClassLibrary/Thread.java
@@ -1,4 +1,5 @@
 public class Thread {
+    private int threadid;
 
     public void start() {
 	nativeCreate();
@@ -7,6 +8,11 @@ public class Thread {
     private static void staticStart(Thread t) {
 	t.run();
     }
+
+    public void join() {
+    }
+
+    private native void nativeJoin();
 
     public native static void sleep(long millis);
     
