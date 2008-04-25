739e62fa058600258797cbda2251804bba7519ff
bdemsky <bdemsky>
2008-04-25 21:49:17 +0000

    Debug Barrier

---
+++ Robust/src/Benchmarks/Prefetch/Moldyn/dsm/DebugBarrier.java
@@ -0,0 +1,54 @@
+public class Barrier {
+  int numthreads;
+  int entercount;
+  boolean cleared;
+
+  public Barrier(int n) {
+      System.printString("Initializing barrier for "+n+" threads.\n");
+    numthreads=n;
+    cleared = false;
+    entercount = 0;
+  }
+
+  public Barrier() {
+  }
+
+  public void reset() {
+    cleared = false;
+    entercount = 0;
+  }
+
+  public static void enterBarrier(Barrier b) {
+    int tmp;
+    boolean retry=true;
+
+    do {
+      atomic {
+	  System.printString("Entering barrier with ec="+b.entercount+" cl="+b.cleared+"\n");
+        if (!b.cleared) {
+          b.entercount++;
+          tmp = b.entercount;
+          if (tmp==b.numthreads) {
+            if(b.numthreads > 1)
+              b.cleared=true;
+            b.entercount--;
+            return;
+          }
+          retry=false;
+        }
+      }
+    } while(retry);
+
+    while(true) {
+      atomic {
+        if (b.cleared) {
+          b.entercount--;
+          int count = b.entercount;
+          if (count==0)
+            b.cleared=false;
+          return;
+        }
+      }
+    }
+  }
+}
