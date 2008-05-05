598dd3458b989f31e9c705f6705129400f6fc62e
bdemsky <bdemsky>
2008-05-05 04:45:20 +0000

    add linked list traversal benchmark

---
+++ Robust/src/Benchmarks/Prefetch/Chase/Chase.java
@@ -0,0 +1,38 @@
+public class Foo {
+    public Foo() {
+	next=null;
+    }
+    Foo next;
+}
+
+public class Chase extends Thread {
+    Foo base;
+    public Chase(Foo b) {
+	base=b;
+    }
+    
+    public static void main(String [] argv) {
+	Chase c;
+	atomic {
+	    Foo fold=global new Foo();
+	    
+	    for(int i=0;i<10000;i++) {
+		Foo f=global new Foo();
+		f.next=fold;
+		fold=f;
+	    }
+	    
+	    c=global new Chase(fold);
+	}
+	c.start((128<<24)|(195<<16)|(175<<8)|71);
+	c.join();
+    }
+    
+    public void run() {
+        atomic {
+	    Foo b=base;
+	    while(b!=null)
+		b=b.next;
+        }
+    }
+}
