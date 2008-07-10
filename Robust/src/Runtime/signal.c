0e38f81c3b8001580aa81fd1723cbe7d82e0dc7a
adash <adash>
2008-07-10 01:03:02 +0000

    changes not to break the compiler

--- Robust/src/Runtime/signal.c
+++ Robust/src/Runtime/signal.c
@@ -15,13 +15,13 @@ void transStatsHandler(int sig, siginfo_t* info, void *context) {
 #endif
 }
 
-#ifdef TRANSSTATS
 void CALL00(___Signal______nativeSigAction____) {
+#ifdef TRANSSTATS
   struct sigaction siga;
   siga.sa_handler = NULL;
   siga.sa_flags = SA_SIGINFO;
   siga.sa_sigaction = &transStatsHandler;
   sigemptyset(&siga.sa_mask);
   sigaction(SIGUSR1, &siga, 0);
-}
 #endif
+}
