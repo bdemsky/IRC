7d581938867627a187071e887332a61c43c7f24a
bdemsky <bdemsky>
2008-08-18 23:32:45 +0000

    code

--- Robust/src/IR/Flat/BuildCode.java
+++ Robust/src/IR/Flat/BuildCode.java
@@ -1500,7 +1500,7 @@ public class BuildCode {
 	    }
 	    if (tuplecount==0)
 		return;
-	    System.out.println("Adding prefetch to method:" +fm);
+	    System.out.println("Adding prefetch "+fpn+ " to method:" +fm);
 	    output.println("{");
 	    output.println("/* prefetch */");
         output.println("/* prefetchid_" + fpn.siteid + " */");
