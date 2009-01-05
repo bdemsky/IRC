dccac30557d7110728c84ea80103d18e0fed6d50
navid <navid>
2009-01-05 18:29:07 +0000

    *** empty log message ***

---
+++ Robust/Transactions/jcarderbenchmarks/modelview/Main.java
@@ -0,0 +1,31 @@
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int timeToSleep;
+        if (args.length > 0) {
+            timeToSleep = Integer.parseInt(args[0]);
+        } else {
+            timeToSleep = 0;
+        }
+
+        Model dataModel = new Model();
+
+        @SuppressWarnings("unused")
+        View view1 = new View(dataModel);
+        View view2 = new View(dataModel);
+        dataModel.setDataAsynchronously();
+        Thread.sleep(timeToSleep);
+
+        // For some (here unspecified) reason, we want to freeze view2
+        // so that it doesn't receive any notifications about changed
+        // data.
+        view2.freeze();
+        System.out.println("sfg");
+        // Now we want notifications about updates again.
+        view2.thaw();
+        
+
+        System.out.println("Main thread finished");
+    }
+}
---
+++ Robust/Transactions/jcarderbenchmarks/modelview/View.java
@@ -0,0 +1,33 @@
+
+
+public class View implements Model.Listener {
+    private Model mDataModel;
+    private boolean mFrozen = false;
+
+    public View(Model dataModel) {
+       mDataModel = dataModel;
+       System.out.println("Registering view in data model");
+       dataModel.registerListener(this);
+    }
+
+    synchronized void freeze() {
+        if (!mFrozen) {
+           System.out.println("Freezing view");
+           mDataModel.unregisterListener(this);
+            System.out.println("Freezing view2");
+           mFrozen = true;
+       }
+    }
+
+    synchronized void thaw() {
+       if (mFrozen) {
+           System.out.println("Thawing view");
+           mDataModel.registerListener(this);
+           mFrozen = false;
+       }
+    }
+
+     synchronized public void modelUpdated() {
+         // ...
+     }
+}
