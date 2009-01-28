db08e90ba058c9d0ab3d80a723410f22a5492c74
navid <navid>
2009-01-28 19:05:07 +0000

    *** empty log message ***

--- Robust/Transactions/TransactionalIO/src/TransactionalIO/benchmarks/thread2.java
+++ Robust/Transactions/TransactionalIO/src/TransactionalIO/benchmarks/thread2.java
@@ -7,6 +7,9 @@ package TransactionalIO.benchmarks;
 
 import TransactionalIO.core.TransactionalFile;
 import TransactionalIO.interfaces.TransactionalProgram;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,22 +18,26 @@ import TransactionalIO.interfaces.TransactionalProgram;
 public class thread2 implements TransactionalProgram{
 
     public void execute() {
-        TransactionalFile tf1 = (TransactionalFile) benchmark.TransactionalFiles.get("0");
-        //TransactionalFile tf1 = new TransactionalFile("/home/navid/output.text", "rw");
-        byte[] b = new byte[20];
-       // tf1.seek(20);
-        int i = tf1.read(b);
-        i = tf1.read(b);
-        i = tf1.read(b);
-        i = tf1.read(b);
-           i = tf1.read(b);
-              i = tf1.read(b);
-                 i = tf1.read(b);
-                    i = tf1.read(b);
-                       i = tf1.read(b);
-                          i = tf1.read(b);
-                             i = tf1.read(b);
-                                i = tf1.read(b);
+        try {
+            TransactionalFile tf1 = (TransactionalFile) benchmark.TransactionalFiles.get("0");
+            //TransactionalFile tf1 = new TransactionalFile("/home/navid/output.text", "rw");
+            byte[] b = new byte[20];
+            // tf1.seek(20);
+            int i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+            i = tf1.read(b);
+        } catch (IOException ex) {
+            Logger.getLogger(thread2.class.getName()).log(Level.SEVERE, null, ex);
+        }
                                 
 
     }
