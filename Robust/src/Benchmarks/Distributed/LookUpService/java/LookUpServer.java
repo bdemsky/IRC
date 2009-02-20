2ccfe0fb1e158ba704f33b4534c187c48d5544a4
adash <adash>
2009-02-20 20:19:36 +0000

    new files...still need work

---
+++ Robust/src/Benchmarks/Distributed/LookUpService/java/LookUpServer.java
@@ -0,0 +1,19 @@
+public class LookUpServer extends Thread {
+  public LookUpServer(Socket s) {
+
+  }
+  
+  public void run() {
+    //Do lookup
+    //Do update
+  }
+
+  public void doHashLookUp() {
+
+
+  }
+
+  public void updateHash() {
+
+  }
+}
