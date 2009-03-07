fd16df8000aed3776073967ab41079825e1789d8
jjenista <jjenista>
2009-03-07 00:45:35 +0000

    changes

--- Robust/src/ClassLibrary/SocketInputStream.java
+++ Robust/src/ClassLibrary/SocketInputStream.java
@@ -16,6 +16,24 @@ public class SocketInputStream extends InputStream {
     return s.read(b);
   }
 
+  public int readAll(byte[] b) {
+      int offset=read(b);
+      if (offset<0)
+	  return offset;
+      int toread=b.length-offset;
+      while(toread>0) {
+	  byte t=new byte[toread];
+	  int rd=read(t);
+	  if (rd<0)
+	      return rd;
+	  for(int i=0;i<rd;i++)
+	      b[i+offset]=t[i];
+	  offset+=rd;
+	  toread-=rd;
+      }
+      return b.length;
+  }
+
   public void close() {
     s.close();
   }
