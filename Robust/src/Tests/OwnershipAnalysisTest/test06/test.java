36345f16fee62230ebd033ce39ecfe310b412458
jjenista <jjenista>
2009-03-12 21:12:10 +0000

    another test

---
+++ Robust/src/Tests/OwnershipAnalysisTest/test06/test.java
@@ -0,0 +1,22 @@
+
+public class Foo {
+  public Foo f;
+  public Foo g;
+  public Foo() {}
+}
+
+
+public class Test {
+
+  static public void main( String[] args ) {
+    Foo a = disjoint A new Foo();
+
+    carolina( a );
+  }
+
+  static public void carolina( Foo p1 ) {
+    Foo z = disjoint Z new Foo();
+
+    p1.f = z;
+  }
+}
