e7522c5c7b3a01afff1ec18f6dde9504e2a4aac2
bdemsky <bdemsky>
2009-04-06 07:00:30 +0000

    STM versions

---
+++ Robust/src/ClassLibrary/JavaSTM/Object.java
@@ -0,0 +1,28 @@
+public class Object {
+  public int cachedCode;   //first field has to be a primitive
+  public boolean cachedHash;
+
+  public native int nativehashCode();
+
+  public int hashCode() {
+    if (!cachedHash) {
+      cachedCode=nativehashCode();
+      cachedHash=true;
+    }
+    return cachedCode;
+  }
+
+  /* DON'T USE THIS METHOD UNLESS NECESSARY */
+  /* WE WILL DEPRECATE IT AS SOON AS INSTANCEOF WORKS */
+  public native int getType();
+
+  public String toString() {
+    return "Object"+hashCode();
+  }
+
+  public boolean equals(Object o) {
+    if (o==this)
+      return true;
+    return false;
+  }
+}
