bb77986eeaa1844f1ba204bec5b6d697e2432917
bdemsky <bdemsky>
2009-07-02 04:03:53 +0000

    make 3d java version...uses 3d arrays...

---
+++ Robust/src/Benchmarks/SingleTM/Labyrinth3D/Grid.java
@@ -0,0 +1,253 @@
+/* =============================================================================
+ *
+ * grid.java
+ *
+ * =============================================================================
+ *
+ * Copyright (C) Stanford University, 2006.  All Rights Reserved.
+ * Author: Chi Cao Minh
+ *
+ * =============================================================================
+ *
+ * For the license of bayes/sort.h and bayes/sort.c, please see the header
+ * of the files.
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of kmeans, please see kmeans/LICENSE.kmeans
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of ssca2, please see ssca2/COPYRIGHT
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of lib/mt19937ar.c and lib/mt19937ar.h, please see the
+ * header of the files.
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of lib/rbtree.h and lib/rbtree.c, please see
+ * lib/LEGALNOTICE.rbtree and lib/LICENSE.rbtree
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * Unless otherwise noted, the following license applies to STAMP files:
+ * 
+ * Copyright (c) 2007, Stanford University
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ * 
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ * 
+ *     * Neither the name of Stanford University nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY STANFORD UNIVERSITY ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL STANFORD UNIVERSITY BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * =============================================================================
+ */
+
+import java.lang.Long;
+
+#define GRID_POINT_FULL -2
+#define GRID_POINT_EMPTY -1
+
+public class Grid {
+    public int width;
+    public int height;
+    public int depth;
+    public int[][][] points_unaligned;
+
+    public Grid() {}
+
+    
+/* =============================================================================
+ * grid_alloc
+ * =============================================================================
+    grid_t* grid_alloc (long width, long height, long depth);
+
+    well... need to implement
+    got stuck
+*/
+  public static Grid alloc(int width,int height,int depth) {
+    Grid grid = new Grid();
+    
+    if(grid != null) {
+      grid.width = width;
+      grid.height = height;
+      grid.depth = depth;
+      
+      int[][][] points_unaligned = new int[width][height][depth];
+
+      
+      for(int i=0;i<width;i++)
+	for(int j=0;j<height;j++)
+	  for(int k=0;k<depth;k++)
+	    points_unaligned[i][j][k]= GRID_POINT_EMPTY;
+      
+      grid.points_unaligned = points_unaligned;
+    }
+    
+    return grid;         
+  }
+
+  public static Grid scratchalloc(int width,int height,int depth) {
+    Grid grid = scratch new Grid();
+    grid.width = width;
+    grid.height = height;
+    grid.depth = depth;
+    int[][][] points_unaligned = scratch new int[width][height][depth];
+    grid.points_unaligned = points_unaligned;
+    return grid;         
+  }
+
+
+
+/* =============================================================================
+ * grid_copy
+ * =============================================================================
+    void grid_copy (grid_t* dstGridPtr, grid_t* srcGridPtr);
+ */
+  public static void copy(Grid dstGridPtr,Grid srcGridPtr) {
+    if((srcGridPtr.width == dstGridPtr.width) ||
+       (srcGridPtr.height == dstGridPtr.height) ||
+       (srcGridPtr.depth == dstGridPtr.depth)) {
+      int width=srcGridPtr.width;
+      int height=srcGridPtr.height;
+      int depth=srcGridPtr.depth;
+      System.deepArrayCopy(dstGridPtr.points_unaligned, srcGridPtr.points_unaligned);
+    }      
+  }
+
+/* =============================================================================
+ * grid_isPointValid
+ * =============================================================================
+ bool_t grid_isPointValid (grid_t* gridPtr, long x, long y, long z);
+ */
+  public boolean isPointValid(int x,int y,int z) {
+    return x>=0 && x< width && y>=0 && y<height && z>=0 && z<depth;
+  }
+
+
+  public int getPoint(int x,int y,int z) {
+    return this.points_unaligned[x][y][z];
+  }
+
+
+/* =============================================================================
+ * grid_isPointEmpty
+ * =============================================================================
+ bool_t grid_isPointEmpty (grid_t* gridPtr, long x, long y, long z); {
+ */
+
+  public boolean isPointEmpty(int x,int y,int z) {
+    return points_unaligned[x][y][z]==GRID_POINT_EMPTY;
+  }
+
+
+
+/* =============================================================================
+ * grid_isPointFull
+ * =============================================================================
+ bool_t grid_isPointFull (grid_t* gridPtr, long x, long y, long z);
+ */
+  public boolean isPointFull(int x,int y,int z) {
+    return points_unaligned[x][y][z]==GRID_POINT_FULL;
+  }
+
+
+/* =============================================================================
+ * grid_setPoint
+ * =============================================================================
+ void grid_setPoint (grid_t* gridPtr, long x, long y, long z, long value);
+ */
+  public void setPoint(int x,int y,int z,int value) {
+    points_unaligned[x][y][z] = value;
+  }
+
+
+/* =============================================================================
+ * grid_addPath
+ * =============================================================================
+ 
+void grid_addPath (grid_t* gridPtr, vector_t* pointVectorPtr);
+*/
+  public void addPath(Vector_t pointVectorPtr) {
+    int i;
+    int n = pointVectorPtr.vector_getSize();
+    
+    for(i = 0; i < n; i++) {
+      Coordinate coordinatePtr = (Coordinate)pointVectorPtr.vector_at(i);
+      int x = coordinatePtr.x;
+      int y = coordinatePtr.y;
+      int z = coordinatePtr.z;
+
+      points_unaligned[x][y][z]=GRID_POINT_FULL;
+    }
+  }
+
+  public boolean TM_addPath(Vector_t pointVectorPtr) {
+    int i;
+    int n = pointVectorPtr.vector_getSize();
+
+    int height = this.height;
+    int width = this.width;
+    int area = height * width;
+    for(i = 1; i < (n-1); i++) {
+      int gridPointIndex = ((Integer)(pointVectorPtr.vector_at(i))).intValue();
+      int z = gridPointIndex / area;
+      int index2d = gridPointIndex % area;
+      int y = index2d / width;
+      int x = index2d % width;        
+      if (points_unaligned[x][y][z] != GRID_POINT_EMPTY) {
+	return true;
+      }
+    }
+
+    for(i = 1; i < (n-1); i++) {
+      int gridPointIndex = ((Integer)(pointVectorPtr.vector_at(i))).intValue();
+      int z = gridPointIndex / area;
+      int index2d = gridPointIndex % area;
+      int y = index2d / width;
+      int x = index2d % width;
+      points_unaligned[x][y][z] = GRID_POINT_FULL;
+    }
+    return false;
+  }
+
+  public int getPointIndex(int x,int y,int z) {
+    return ((z * height) + y) * width + x;
+  }
+
+
+
+}
+
+
+/* =============================================================================
+ *
+ * End of grid.c
+ *
+ * =============================================================================
+ */
---
+++ Robust/src/Benchmarks/SingleTM/Labyrinth3D/Router.java
@@ -0,0 +1,416 @@
+/* =============================================================================
+ *
+ * Router.java
+ *
+ * =============================================================================
+ *
+ * Copyright (C) Stanford University, 2006.  All Rights Reserved.
+ * Author: Chi Cao Minh
+ * 
+ * Ported to Java
+ * Author: Jihoon Lee
+ * University of California, Irvine
+ *
+ * =============================================================================
+ *
+ * For the license of bayes/sort.h and bayes/sort.c, please see the header
+ * of the files.
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of kmeans, please see kmeans/LICENSE.kmeans
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of ssca2, please see ssca2/COPYRIGHT
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of lib/mt19937ar.c and lib/mt19937ar.h, please see the
+ * header of the files.
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * For the license of lib/rbtree.h and lib/rbtree.c, please see
+ * lib/LEGALNOTICE.rbtree and lib/LICENSE.rbtree
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * Unless otherwise noted, the following license applies to STAMP files:
+ * 
+ * Copyright (c) 2007, Stanford University
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ * 
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ * 
+ *     * Neither the name of Stanford University nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY STANFORD UNIVERSITY ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL STANFORD UNIVERSITY BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * =============================================================================
+ */
+
+#define MOMENTUM_ZERO 0
+#define MOMENTUM_POSX 1
+#define MOMENTUM_POSY 2
+#define MOMENTUM_POSZ 3
+#define MOMENTUM_NEGX 4
+#define MOMENTUM_NEGY 5
+#define MOMENTUM_NEGZ 6
+#define GRID_POINT_FULL -2
+#define GRID_POINT_EMPTY -1
+
+public class Router {
+    public int xCost;
+    public int yCost;
+    public int zCost;
+    public int bendCost;
+    public static Point MOVE_POSX;
+    public static Point MOVE_POSY;
+    public static Point MOVE_POSZ;
+    public static Point MOVE_NEGX;
+    public static Point MOVE_NEGY;
+    public static Point MOVE_NEGZ;
+
+    public Router() {}
+
+/* =============================================================================
+ * router_alloc
+ * =============================================================================
+ * router_t* router_alloc (long xCost, long yCost, long zCost, long bendCost);
+ */
+  public static Router alloc(int xCost,int yCost,int zCost,int bendCost) {
+    Router routerPtr = new Router();
+
+    
+    routerPtr.MOVE_POSX = new Point(1,0,0,0,MOMENTUM_POSX);
+    routerPtr.MOVE_POSY = new Point(0,1,0,0,MOMENTUM_POSY);
+    routerPtr.MOVE_POSZ = new Point(0,0,1,0,MOMENTUM_POSZ);
+    routerPtr.MOVE_NEGX = new Point(-1,0,0,0,MOMENTUM_NEGX);
+    routerPtr.MOVE_NEGY = new Point(0,-1,0,0,MOMENTUM_NEGY);
+    routerPtr.MOVE_NEGZ = new Point(0,0,-1,0,MOMENTUM_NEGZ);
+
+    if(routerPtr != null) {
+      routerPtr.xCost = xCost;
+      routerPtr.yCost = yCost;
+      routerPtr.zCost = zCost;
+      routerPtr.bendCost = bendCost;
+    }
+    
+    return routerPtr;    
+  }
+
+
+/* ============================================================================
+ * PexpandToneighbor
+ * ============================================================================
+ */
+  private void PexpandToNeighbor(Grid myGridPtr, 
+				 int x,int y,int z, int value,Queue_Int queuePtr) {
+    if (myGridPtr.isPointValid(x,y,z)) {
+      int neighborValue = myGridPtr.points_unaligned[x][y][z];
+      if (neighborValue == GRID_POINT_EMPTY) {
+	int neighborGridPointIndex = myGridPtr.getPointIndex(x,y,z);
+	myGridPtr.points_unaligned[x][y][z] = value;
+	queuePtr.queue_push(neighborGridPointIndex);
+      } else if (neighborValue != GRID_POINT_FULL) {
+	if (value < neighborValue) {
+	  int neighborGridPointIndex = myGridPtr.getPointIndex(x,y,z);
+	  myGridPtr.points_unaligned[x][y][z] = value;
+	  queuePtr.queue_push(neighborGridPointIndex);
+	}
+      }
+    }
+  }
+
+
+/* ============================================================================
+ * PdoExpansion
+ * ============================================================================
+ */
+    public boolean PdoExpansion (Router routerPtr,Grid myGridPtr,Queue_Int queuePtr,
+                                  Coordinate srcPtr,Coordinate dstPtr) {
+        int xCost = routerPtr.xCost;
+        int yCost = routerPtr.yCost;
+        int zCost = routerPtr.zCost;
+
+        /* 
+         * Potential Optimization: Make 'src' the one closet to edge.
+         * This will likely decrease the area of the emitted wave.
+         */
+
+        queuePtr.queue_clear();
+
+        int srcGridPointIndex = myGridPtr.getPointIndex(srcPtr.x,srcPtr.y,srcPtr.z);
+
+        queuePtr.queue_push(srcGridPointIndex);
+
+        myGridPtr.setPoint(srcPtr.x,srcPtr.y,srcPtr.z,0);
+        myGridPtr.setPoint(dstPtr.x,dstPtr.y,dstPtr.z,GRID_POINT_EMPTY);
+        int dstGridPointIndex = myGridPtr.getPointIndex(dstPtr.x,dstPtr.y,dstPtr.z);
+        boolean isPathFound = false;
+	int height = myGridPtr.height;
+	int width = myGridPtr.width;
+	int area = height * width;
+        while (!queuePtr.queue_isEmpty()) {
+            int gridPointIndex = queuePtr.queue_pop();
+
+            if(gridPointIndex == dstGridPointIndex) {
+	      isPathFound = true;
+	      break;
+            }
+	    
+	    int z = gridPointIndex / area;
+	    int index2d = gridPointIndex % area;
+	    int y = index2d / width;
+	    int x = index2d % width;        
+	    int value = myGridPtr.points_unaligned[x][y][z];
+	    
+            /*
+             * Check 6 neighbors
+             *
+             * Potential Optimization: Only need to check 5 of these
+             */
+	    PexpandToNeighbor(myGridPtr, x+1, y,   z,   (value + xCost), queuePtr);
+	    PexpandToNeighbor(myGridPtr, x-1, y,   z,   (value + xCost), queuePtr);
+	    PexpandToNeighbor(myGridPtr, x, y+1,   z,   (value + yCost), queuePtr);
+	    PexpandToNeighbor(myGridPtr, x, y-1,   z,   (value + yCost), queuePtr);   
+	    PexpandToNeighbor(myGridPtr, x, y,   z+1,   (value + zCost), queuePtr);
+	    PexpandToNeighbor(myGridPtr, x, y,   z-1,   (value + zCost), queuePtr);
+	    
+        } /* iterate over work queue */
+	
+        return isPathFound;
+    }
+            
+            
+/* ============================================================================
+ * traceToNeighbor
+ * ============================================================================
+ */
+    private void traceToNeighbor(Grid myGridPtr,
+                                 Point currPtr,
+                                 Point movePtr,
+                                 boolean useMomentum,
+                                 int bendCost,
+                                 Point nextPtr)
+    {
+      int x = currPtr.x + movePtr.x;
+      int y = currPtr.y + movePtr.y;
+      int z = currPtr.z + movePtr.z;
+
+        if (myGridPtr.isPointValid(x,y,z) &&
+                !myGridPtr.isPointEmpty(x,y,z) &&
+                !myGridPtr.isPointFull(x,y,z))
+        {
+            int value = myGridPtr.getPoint(x,y,z);
+            int b = 0;
+            
+            if (useMomentum && (currPtr.momentum != movePtr.momentum)) {
+                b = bendCost;
+            }
+            if ((value + b) <= nextPtr.value) { /* '=' favors neighbors over current */
+                nextPtr.x = x;
+                nextPtr.y = y;
+                nextPtr.z = z;
+                nextPtr.value = value;
+                nextPtr.momentum = movePtr.momentum;
+            }
+        }
+    }
+/* =============================================================================
+ * PdoTraceback
+ * =============================================================================
+ */
+
+    private Vector_t PdoTraceback(Grid myGridPtr,
+                                  Coordinate dstPtr, int bendCost) {
+        Vector_t pointVectorPtr = Vector_t.vector_alloc(1);
+
+        Point next = new Point();
+        next.x = dstPtr.x;
+        next.y = dstPtr.y;
+        next.z = dstPtr.z;
+        next.value = myGridPtr.getPoint(next.x,next.y,next.z);
+        next.momentum = MOMENTUM_ZERO;
+
+        while(true) {
+            int gridPointIndex = myGridPtr.getPointIndex(next.x,next.y,next.z);
+            pointVectorPtr.vector_pushBack(new Integer(gridPointIndex));
+            myGridPtr.setPoint(next.x,next.y,next.z,GRID_POINT_FULL);
+
+            /* Check if we are done */
+            if (next.value == 0) {
+                break;
+            }
+            Point curr = new Point();
+            curr.x = next.x;
+            curr.y = next.y;
+            curr.z = next.z;
+            curr.value = next.value;
+            curr.momentum = next.momentum;
+
+            /*
+             * Check 6 neibors
+             */
+
+            traceToNeighbor(myGridPtr,curr,MOVE_POSX,true, bendCost, next);
+            traceToNeighbor(myGridPtr,curr,MOVE_POSY,true, bendCost, next);   
+            traceToNeighbor(myGridPtr,curr,MOVE_POSZ,true, bendCost, next);
+            traceToNeighbor(myGridPtr,curr,MOVE_NEGX,true, bendCost, next); 
+            traceToNeighbor(myGridPtr,curr,MOVE_NEGY,true, bendCost, next);           
+            traceToNeighbor(myGridPtr,curr,MOVE_NEGZ,true, bendCost, next);          
+            /* 
+             * Because of bend costs, none of the neighbors may appear to be closer.
+             * In this case, pick a neighbor while ignoring momentum.
+             */
+
+
+            
+            if ((curr.x == next.x) &&
+                (curr.y == next.y) &&
+                (curr.z == next.z)) {
+                next.value = curr.value;
+                traceToNeighbor(myGridPtr,curr,MOVE_POSX,false, bendCost, next);
+                traceToNeighbor(myGridPtr,curr,MOVE_POSY,false, bendCost, next);
+                traceToNeighbor(myGridPtr,curr,MOVE_POSZ,false, bendCost, next);
+                traceToNeighbor(myGridPtr,curr,MOVE_NEGX,false, bendCost, next);
+                traceToNeighbor(myGridPtr,curr,MOVE_NEGY,false, bendCost, next);
+                traceToNeighbor(myGridPtr,curr,MOVE_NEGZ,false, bendCost, next);
+                
+                if ((curr.x == next.x) &&
+                    (curr.y == next.y) &&
+                    (curr.z == next.z)) {
+                    System.out.println("Dead");
+                    return null;
+                }
+            }
+        }
+
+        return pointVectorPtr;
+    }
+
+/* =============================================================================
+ * router_solve
+ * =============================================================================
+ * void router_solve (void* argPtr);
+ */
+    public static void solve(Object argPtr) 
+    {
+        // TM_THREAD_ENTER();
+        //
+        Solve_Arg routerArgPtr = (Solve_Arg) argPtr;
+        Router routerPtr = routerArgPtr.routerPtr;
+        Maze mazePtr = routerArgPtr.mazePtr;
+        Vector_t myPathVectorPtr = Vector_t.vector_alloc(1);
+
+        Queue_t workQueuePtr = mazePtr.workQueuePtr;
+        Grid gridPtr = mazePtr.gridPtr;
+        Grid myGridPtr = Grid.scratchalloc(gridPtr.width,gridPtr.height,gridPtr.depth);
+        int bendCost = routerPtr.bendCost;
+        Queue_Int myExpansionQueuePtr = Queue_Int.queue_alloc(-1);
+
+        /*
+         * Iterate over work list to route each path. This involves an
+         * 'expansion' and 'tracback' phase for each source/destination pair.
+         */
+        while(true) {
+            Pair coordinatePairPtr;
+            
+            // TM_BEGIN();
+            atomic {
+                if(workQueuePtr.queue_isEmpty()) {
+                    coordinatePairPtr = null;
+                } else {
+                    coordinatePairPtr = (Pair)workQueuePtr.queue_pop();
+                }
+            }
+            // TM_END();
+            //
+            
+            if (coordinatePairPtr == null) {
+                break;
+            }
+
+            Coordinate srcPtr = (Coordinate)coordinatePairPtr.first;
+            Coordinate dstPtr = (Coordinate)coordinatePairPtr.second;
+
+//            System.out.println("SRC x = " + srcPtr.x + "  y = " + srcPtr.y + " z = " +srcPtr.z);
+            
+            boolean success = false;
+            Vector_t pointVectorPtr = null;
+	    boolean retry=true;
+
+            // TM_BEGIN();
+	    while(retry) {
+	      retry=false;
+	      atomic {
+		Grid.copy(myGridPtr, gridPtr); /* ok if not most up-to-date */
+		if(routerPtr.PdoExpansion(routerPtr,myGridPtr,myExpansionQueuePtr,srcPtr,dstPtr)) {
+		  pointVectorPtr = routerPtr.PdoTraceback(myGridPtr,dstPtr,bendCost);
+		  if (pointVectorPtr != null) {
+		    if (gridPtr.TM_addPath(pointVectorPtr)) {
+		      pointVectorPtr=null;
+		      retry=true;
+		    } else
+		      success=true;
+		  }
+		}
+	      }
+	    }
+	    
+            if(success) {
+                boolean status = myPathVectorPtr.vector_pushBack(pointVectorPtr);
+                
+                if(!status) {
+                    System.out.println("Assert in Router_Solve");
+                    System.exit(1);
+                }
+            }
+        }
+
+        /*
+         * Add my paths to global list
+         */
+        List_t pathVectorListPtr = routerArgPtr.pathVectorListPtr;
+
+        atomic {
+            pathVectorListPtr.insert(myPathVectorPtr);
+        }
+
+        myGridPtr = null;
+        myExpansionQueuePtr = null;
+//        System.out.println("Final Grid: ");
+//        gridPtr.print();
+        
+        // TM_THREAD_EXIT();
+    }
+}
+/* =============================================================================
+ *
+ * End of router.java
+ * 
+ * =============================================================================
+ */
