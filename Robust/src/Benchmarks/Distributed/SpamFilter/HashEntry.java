a174c3177eba6f7685f8be548a502c67ed2a269a
adash <adash>
2009-10-30 00:30:17 +0000

    initial checkin

---
+++ Robust/src/Benchmarks/Distributed/SpamFilter/HashEntry.java
@@ -0,0 +1,45 @@
+public class HashEntry {
+  String engine;
+  String signature;
+  HashStat stats;
+  public HashEntry() {
+  }
+
+  /**
+   * hashCode that combines two strings using xor.
+   * @return a hash code value on the entire object.
+   */
+  public int hashCode() {
+    int result=0;
+    // this will not work well if some of the strings are equal.
+    result = engine.hashCode();
+    result ^= signature.hashCode();
+    result ^= stats.hashCode();
+    return result;
+  }
+
+  public String getEngine() {
+    return engine;
+  }
+
+  public String getSignature() {
+    return signature;
+  }
+
+  public Stat getStats() {
+    return stats;
+  }
+
+  public boolean equals(Object o) {
+    if(o.getType()!=getType())
+      return false;
+    HashEntry he = (HashEntry)o;
+    if(!(he.getEngine().equals(Engine)))
+      return false;
+    if(!(he.getSignature().equals(Signature)))
+      return false;
+    if(!(he.getStats().equals(stats)))
+      return false;
+    return true;
+  }
+}
---
+++ Robust/src/Benchmarks/Distributed/SpamFilter/HashStat.java
@@ -0,0 +1,27 @@
+public class HashStat {
+  int[] userid;
+  FilterStatistic[] userstat; 
+  public HashStat() {
+    userid = new int[8]; //max users for our system=8
+    userstat = new FilterStatistic[8];
+    for(int i=0; i<8; i++) {
+      userstat[i] = new FilterStatistic();
+    }
+  }
+
+  public int getuser(int id) {
+    return userid[id];
+  }
+
+  public int getspamcount(int userid) {
+    return userstat[userid].getSpam();
+  }
+
+  public int gethamcount(int userid) {
+    return userstat[userid].getham();
+  }
+
+  public int getunknowncount(int userid) {
+    return userstat[userid].getUnknown();
+  }
+}
