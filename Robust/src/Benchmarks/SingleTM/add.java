88a415c71df2389e802eaf38fc8f9a768b1e6b8f
bdemsky <bdemsky>
2009-11-16 07:56:13 +0000

    current versions for generating all graphs

--- Robust/src/Benchmarks/SingleTM/add.java
+++ Robust/src/Benchmarks/SingleTM/add.java
@@ -4,11 +4,14 @@ public class add {
       int ii=0;
       boolean outputerr=false;      
       boolean normalize=false;
+      boolean timeinsec=false;
       double norma=0.0;
       for(;ii<args.length;ii++) {
 	if (args[ii].equals("-err"))
 	  outputerr=true;
-	else if (args[ii].equals("-norm")) {
+	else if (args[ii].equals("-sec")) {
+	  timeinsec=true;
+	} else if (args[ii].equals("-norm")) {
 	  normalize=true;
 	  ii++;
 	  norma=Double.parseDouble(args[ii]);
@@ -26,8 +29,44 @@ public class add {
 	while((nextline=br.readLine())!=null) {
 	  double v;
 	  int start=nextline.indexOf("TIME=")+5;
-	  String num=nextline.substring(start, nextline.length());
+
+	  if (start==4) {
+	    start=nextline.indexOf("Time: ")+6;
+	    if (start==5) {
+	      start=nextline.indexOf("Time = ")+7;
+	      if (start==6) {
+		start=nextline.indexOf("Time taken for kernel 1 is  ")+28;
+		if (start==27) {
+		  start=nextline.indexOf("Time taken for kernel 1 is ")+27;
+		  if (start==26) {
+		    start=nextline.indexOf("Elapsed time    = ")+18;
+		    if (start==17) {
+		      start=nextline.indexOf("Elapsed time                    = ");
+		      if (start==-1) {
+			start=nextline.indexOf("Learn time = ");
+			if (start==-1)
+			  continue;
+			else
+			  start+=(new String("Learn time = ")).length();
+		      } else {
+			start+=(new String("Elapsed time                    = ")).length();
+		      }
+		    }
+		  }
+		}
+	      }
+	    }
+	  }
+	  
+	  nextline=nextline.substring(start, nextline.length());
+	  int lastindex=nextline.indexOf(' ');
+	  if (lastindex==-1)
+	    lastindex=nextline.length();
+	  String num=nextline.substring(0, lastindex);
 	  v=Double.parseDouble(num);
+	  if (timeinsec)
+	    v=v*1000;
+
 	  if (normalize)
 	    v=norma/v;
 	  vals[numvals++]=v;
