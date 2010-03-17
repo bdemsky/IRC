038cafb68554557b57bc937113c151290b60fc87
yeom <yeom>
2010-03-17 18:51:07 +0000

    a test

---
+++ Robust/src/Tests/disjoint/crashTest1/test.java
@@ -0,0 +1,17 @@
+public class Foo {
+  public Foo() {}
+  public Foo f;
+}
+
+public class Test {
+  static public void main( String[] args ) {
+    Foo a = disjoint A new Foo();
+    f1(a);
+  }
+     
+  static public void f1( Foo c ) {
+    Foo d = new Foo();
+    c.f = d;
+  }
+    
+}
