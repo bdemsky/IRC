68a08bb8bf03f2baac43c4a9b8957893ab9622e4
stephey <stephey>
2010-04-02 18:48:23 +0000

    This is Stephen's test program

---
+++ Robust/src/Tests/mlp/stephen/test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public Test(){}
+    
+    public static void main(String args[]) {
+	
+	System.out.println("# it starts");	
+	Test t = new Test();
+	t.doSomeWork();
+	
+    }    
+    
+    public void doSomeWork() {
+
+    }
+   
+}
+
