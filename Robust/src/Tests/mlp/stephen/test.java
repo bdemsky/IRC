fb34cbe7c85b68ec0ae39e441ede95c0213f7faf
stephey <stephey>
2010-04-02 18:54:38 +0000

    A test update

--- Robust/src/Tests/mlp/stephen/test.java
+++ Robust/src/Tests/mlp/stephen/test.java
@@ -4,7 +4,7 @@ public class Test {
     
     public static void main(String args[]) {
 	
-	System.out.println("# it starts");	
+	System.out.println("# it starts!!");	
 	Test t = new Test();
 	t.doSomeWork();
 	
