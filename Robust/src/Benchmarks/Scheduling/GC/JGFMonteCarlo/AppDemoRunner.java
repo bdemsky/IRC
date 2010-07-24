44a9b43e3162cdedfda62cbf9e97fa10cb2e7982
jzhou <jzhou>
2010-07-24 23:25:15 +0000

    add the MonteCarlo benchmark for multicore gc

---
+++ Robust/src/Benchmarks/Scheduling/GC/JGFMonteCarlo/AppDemoRunner.java
@@ -0,0 +1,66 @@
+class AppDemoRunner {
+    flag run;
+    flag turnin;
+    
+    public String header;
+    public String name;
+    public int startDate;
+    public int endDate;
+    public float dTime;
+    public int returnDefinition;
+    public float expectedReturnRate;
+    public float volatility;
+    public int nTimeSteps;
+    public float pathStartValue;
+
+    int id, nRunsMC, group;
+    //ToInitAllTasks toinitalltasks;
+    public Vector results;
+
+    public AppDemoRunner(int id,
+	                 int nRunsMC, 
+	                 int group, 
+	                 AppDemo ad
+	                 /*ToInitAllTasks initalltask*/) {
+        this.id = id;
+        this.nRunsMC=nRunsMC;
+        this.group = group;
+        this.results = new Vector();
+        
+        //this.header = initalltask.header;
+        this.name = ad.name;
+        this.startDate = ad.startDate;
+        this.endDate = ad.endDate;
+        this.dTime = ad.dTime;
+        this.returnDefinition = ad.returnDefinition;
+        this.expectedReturnRate = ad.expectedReturnRate;
+        this.volatility = ad.volatility;
+        this.nTimeSteps = ad.nTimeStepsMC;
+        this.pathStartValue = ad.pathStartValue;
+    }
+
+    public void run() {
+        // Now do the computation.
+        int ilow, iupper, slice;
+        int gp = this.group;
+        int index = this.id;
+        int nruns = this.nRunsMC;
+
+        slice = (nruns + gp-1)/gp;
+
+        ilow = index*slice;
+        iupper = (index+1)*slice;
+        if (index==gp-1) {
+            iupper=nruns;
+        }
+
+        for(int iRun=ilow; iRun < iupper; iRun++ ) {
+            //String header="MC run "+String.valueOf(iRun);
+            PriceStock ps = new PriceStock();
+            ps.setInitAllTasks(this);
+            ps.setTask(/*header, */(long)iRun*11);
+            ps.run();
+            results.addElement(ps.getResult());
+        }
+    }
+}
---
+++ Robust/src/Benchmarks/Scheduling/GC/JGFMonteCarlo/JGFMonteCarloBench.java
@@ -0,0 +1,73 @@
+/** Banboo Version  **/
+
+/**************************************************************************
+*                                                                         *
+*         Java Grande Forum Benchmark Suite - Thread Version 1.0          *
+*                                                                         *
+*                            produced by                                  *
+*                                                                         *
+*                  Java Grande Benchmarking Project                       *
+*                                                                         *
+*                                at                                       *
+*                                                                         *
+*                Edinburgh Parallel Computing Centre                      *
+*                                                                         * 
+*                email: epcc-javagrande@epcc.ed.ac.uk                     *
+*                                                                         *
+*                                                                         *
+*      This version copyright (c) The University of Edinburgh, 2001.      *
+*                         All rights reserved.                            *
+*                                                                         *
+**************************************************************************/
+
+task t1(StartupObject s{initialstate}) {
+    //System.printString("task t1\n");
+    
+    int datasize = 10000;  //should be times of 2
+    int nruns = 62 * 62;  //16 * 16;
+    int group = 62; // 16;
+    
+    AppDemo ad = new AppDemo(datasize, nruns, group){merge};
+    
+    ad.initSerial();
+    
+    for(int i = 0; i < group; i++) {
+	AppDemoRunner adr = new AppDemoRunner(i, 
+		                              nruns, 
+		                              group, 
+		                              ad){run};
+    }
+    
+    taskexit(s{!initialstate});
+}
+
+task t2(AppDemoRunner adr{run}) {
+    //System.printString("task t2\n");
+    
+    //  Now do the computation.
+    adr.run();
+    
+    taskexit(adr{!run, turnin});
+}
+
+task t3(AppDemo ad{merge}, AppDemoRunner adr{turnin}) {
+    //System.printString("task t3\n");
+    boolean isFinish = ad.processResults(adr.results);
+    
+    if(isFinish) {
+	taskexit(ad{!merge, validate}, adr{!turnin});
+    }
+    taskexit(adr{!turnin});
+}
+
+task t4(AppDemo ad{validate}) {
+    //System.printString("task t4\n");
+    float refval = (float)(-0.0333976656762814);
+    float dev = Math.abs(ad.JGFavgExpectedReturnRateMC - refval);
+    if (dev > 1.0e-12 ){
+      //System.printString("Validation failed");
+      //System.printString(" expectedReturnRate= " + (int)(ad.JGFavgExpectedReturnRateMC*10000) + "  " + (int)(dev*10000) + "\n");
+    }
+    taskexit(ad{!validate});
+}
+
