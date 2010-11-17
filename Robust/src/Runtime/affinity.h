e98aa0573f982ff794415a3a4081fdd2a70965d3
bdemsky <bdemsky>
2010-11-17 07:27:23 +0000

    affinity call

---
+++ Robust/src/Runtime/affinity.h
@@ -0,0 +1,16 @@
+#ifndef AFFINITY_H
+#define AFFINITY_H
+
+static void set_affinity(unsigned long cpu) {
+  int err;
+  cpu_set_t cpumask;
+
+  CPU_ZERO(&cpumask);
+  CPU_SET(cpu, &cpumask);
+  err = sched_setaffinity(syscall(SYS_gettid),
+			  sizeof(cpu_set_t), &cpumask);
+
+  if (err == -1)
+    printf("set_affinity: %s\n", strerror(errno));
+}
+#endif
