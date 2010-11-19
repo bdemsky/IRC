e885d05a46362df486cef0e3939ae8aea9a0f928
bdemsky <bdemsky>
2010-11-19 02:01:57 +0000

    checkin

---
+++ Robust/src/Benchmarks/oooJava/micro/test2.java
@@ -0,0 +1,29 @@
+public class test {
+
+  public static void main( String argv[] ) {
+    
+    long count = 500000;
+    int x=1;
+    
+    if( argv.length > 0 ) {
+      count = count * Integer.parseInt( argv[0] );
+    }
+    
+    if( argv.length > 1 ) {
+      x = Integer.parseInt( argv[1] );
+    }
+    
+    for( long i = 0; i < count; i++ ) {
+      
+      // the parent does a simple variable copy
+      // into this child's record at issue because
+      // the value is definitely available, the
+      // child needs the value read-only
+      rblock child {
+	for(int j=0;j<x;j++) {
+	  ;
+	}
+      }
+    }
+  }
+}
