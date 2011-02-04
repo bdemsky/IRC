844ac2949487feff972173b6115964d04b311855
jzhou <jzhou>
2011-02-04 01:36:33 +0000

    Class library for SPECjbb

---
+++ Robust/src/ClassLibrary/MGC/Object.java
@@ -0,0 +1,38 @@
+public class Object {
+  public int cachedCode;   //first field has to be a primitive
+  public boolean cachedHash;
+
+  public native int nativehashCode();
+  private Object nextlockobject;
+  private Object prevlockobject;
+
+  // temporary extra unused int filed to align objects for Java
+  //int wkhqwemnbmwnb;
+
+  public int hashCode() {
+    if (!cachedHash) {
+      cachedCode=nativehashCode();
+      cachedHash=true;
+    }
+    return cachedCode;
+  }
+
+  /* DON'T USE THIS METHOD UNLESS NECESSARY */
+  /* WE WILL DEPRECATE IT AS SOON AS INSTANCEOF WORKS */
+  public native int getType();
+
+  public native int MonitorEnter();
+  public native int MonitorExit();
+
+  public String toString() {
+    return "Object"+hashCode();
+  }
+
+  public boolean equals(Object o) {
+    if (o==this)
+      return true;
+    return false;
+  }
+  
+  //public final native Class getClass();
+}
---
+++ Robust/src/ClassLibrary/MGC/Thread.java
@@ -0,0 +1,27 @@
+public class Thread {
+  private boolean finished;
+
+  public void start() {
+    nativeCreate();
+  }
+
+  private static void staticStart(Thread t) {
+    t.run();
+  }
+
+  public static native void yield();
+
+  public void join() {
+    nativeJoin();
+  }
+
+  private native void nativeJoin();
+
+  public native static void sleep(long millis);
+
+  public void run() {
+  }
+
+  private native void nativeCreate();
+
+}
