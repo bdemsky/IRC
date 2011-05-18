2f9ab427cc817bcd05c2b0ee43c2c5cf6e9e9f89
yeom <yeom>
2011-05-18 18:59:14 +0000

    add more test cases: delta func and local variable lattice

--- Robust/src/Tests/ssJava/flowdown/test.java
+++ Robust/src/Tests/ssJava/flowdown/test.java
@@ -22,6 +22,8 @@ public class test{
 	fooM.doSomethingArgs(fieldH,fieldM);
 
 	doit2();
+	doOwnLattice();
+	doDelta();
     }
     
     public void doit2(){
@@ -32,7 +34,32 @@ public class test{
 	localVarL=fieldH;
     }
 
+    // method has its own local variable lattice 
+    @LATTICE("mL<mH,THISLOC=mH")
+    public void doOwnLattice(){
+	@LOC("mL") int varL;
+	@LOC("mH") int varH;
+	varL=varH;
+    }
+    
+      
+    @LATTICE("mL<mH,THISLOC=mH")
+    public void doDelta(){
+	@LOC("DELTA(mH,testH)") int varDelta;
+	// LOC(varDelta) is slightly lower than [mH, testH]
+
+	@LOC("DELTA(DELTA(mH,testH))") int varDeltax2;
+	// applying double delta to [mH,testH]
+	
+	varDelta=fieldH; // [mH,testH] -> DELTA[mh,testH]
+	varDeltax2=varDelta; //DELTA[mh,testH] -> DELTA[DELTA[mh,testH]]
+	
+	fieldM=varDeltax2; //  DELTA[DELTA[mh,testH]] -> [mH,testM]	
+    }
+
 }
+
+
 @LATTICE("FC<FB,FB<FA")
 @METHODDEFAULT("fm_L<fm_M1,fm_L<fm_M2,fm_M1<fm_H,fm_M2<fm_H,THISLOC=fm_H")
 class Foo{
