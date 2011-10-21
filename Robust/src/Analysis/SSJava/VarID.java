fa8edd6e07c44e1e498642921b8502552f48bb59
david <david>
2011-10-21 00:42:25 +0000

    adds global, this, and static tags for var id

--- Robust/src/Analysis/SSJava/VarID.java
+++ Robust/src/Analysis/SSJava/VarID.java
@@ -3,48 +3,83 @@ package Analysis.SSJava;
 import java.util.ArrayList;
 import IR.Descriptor;
 
-public class VarID {
+public class VarID{
+    
+    //contains field and var descriptors
+    //given the case a.b.f it contains descriptors for a,b, and f
+    private ArrayList<Descriptor> var;
+    //properties of ID
+    private boolean isThis;
+    private boolean isGlobal;
+    private boolean isTop;
+    private boolean isReturn;
 
-  // contains field and var descriptors
-  // given the case a.b.f it contains descriptors for a,b, and f
-  private ArrayList<Descriptor> var;
-  // properties of ID
-  private boolean isThis;
-  private boolean isGlobal;
-  private boolean isTop;
+    public VarID(){
+	this.var = new ArrayList<Descriptor>();
+	isThis = false;
+	isGlobal = false;
+	isTop = false;
+	isReturn = false;
+    }
+    public VarID(Descriptor var){
+	this.var = new ArrayList<Descriptor>();
+	this.var.add(var);
+	isThis = false;
+	isGlobal = false;
+	isTop = false;
+	isReturn = false;
+    }
+    
+    public void addAccess(Descriptor var){
+	this.var.add(var);
+    }
+    
+    public ArrayList<Descriptor> getDesc(){
+	return var;
+    }
 
-  public VarID(Descriptor var) {
-    this.var = new ArrayList<Descriptor>();
-    this.var.add(var);
-    isThis = false;
-    isGlobal = false;
-    isTop = false;
-  }
+    public void setThis(){
+	isThis = true;
+    }
+    
+    public boolean isThis(){
+	return isThis;
+    }
 
-  public void addAccess(Descriptor var) {
-    this.var.add(var);
-  }
+    public void setGlobal(){
+	isGlobal = true;
+    }
 
-  public void setThis() {
-    isThis = true;
-  }
+    public boolean isGlobal(){
+	return isGlobal;
+    }
 
-  public void setGlobal() {
-    isGlobal = true;
-  }
+    public void setTop(){
+	isTop = true;
+    }
+    
+    public boolean isTop(){
+	return isTop;
+    }
+    
+    public void setReturn(){
+	isReturn = true;
+    }
 
-  public void setTop() {
-    isTop = true;
-  }
+    public boolean isReturn(){
+	return isReturn;
+    }
 
-  public String toString() {
-    String toReturn = "";
-    for (Descriptor d : var)
-      toReturn += d.toString() + " ";
-    return toReturn;
-  }
+    public boolean equals(VarID id){
+	return var.equals(id.getDesc());
+    }
 
-  public void setReturn() {
-    //interim fixes
-  }
+    public String toString(){
+	if(isReturn)
+	    return "RETURN";
+	String toReturn = "";
+	for(Descriptor d: var)
+	    toReturn += d.toString() + " ";
+	return toReturn;
+    }
 }
