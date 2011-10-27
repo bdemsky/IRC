ca7b45b30d2e52d59e65eea4779c783e90b67679
jjenista <jjenista>
2011-10-27 17:39:48 +0000

    exploring error injection space

--- Robust/src/Benchmarks/SSJava/MP3Decoder/plot-normal-vs-error.sh
+++ Robust/src/Benchmarks/SSJava/MP3Decoder/plot-normal-vs-error.sh
@@ -7,26 +7,16 @@ X=nve-diff-ranges.tmp
 echo '0 1' >  $X
 echo '0 0' >> $X
 
-#diff normal.txt error.txt | \
-#sed \
-#-e '/^[^0-9]/ d' \
-#-e 's/\(.*\),\(.*\)c.*/\1 0\n\1 1\n\2 1\n\2 0/' \
-#-e 's/\(.*\)c.*/\1 0\n\1 1\n\1 1\n\1 0/' \
-#>> $X
-
 sed \
 -e '/^[^0-9]/ d' \
 -e 's/\(.*\),\(.*\)c.*/\1 0\n\1 1\n\2 1\n\2 0/' \
 -e 's/\(.*\)c.*/\1 0\n\1 1\n\1 1\n\1 0/' \
 $D >> $X
 
-if [[ -s $D ]] 
-then
+if [[ -s $D ]] ; then
   echo 'Normal and Error files differ.'
 else
   echo 'NO DIFF!'
 fi
 
 gnuplot -persist nve.cmds
-
-rm -f $D $X
